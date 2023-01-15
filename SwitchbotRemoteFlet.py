e84043ac34e606fa2e91d546a6a0b03805a2df76
daemokra <ryoya0126t@gmail.com>
2023-01-15 19:39:49 +0900

    設定温度入力テキストボックス追加

--- SwitchbotRemoteFlet.py
+++ SwitchbotRemoteFlet.py
@@ -24,6 +24,7 @@ class SwitchBotRemote(ft.UserControl):
         self.aircons = self.switchbot.get_airconditioners()
         self.main_ac = self.aircons[0]
         self.ac_mode_icon = ft.Icon(name=self.get_mode_icon(), size=15)
+
         # エアコンの設定値ディスプレイ
         self.ac_display = ft.Container(
             border_radius = ft.border_radius.all(10),
@@ -31,15 +32,36 @@ class SwitchBotRemote(ft.UserControl):
             margin  = 5,
             bgcolor=ft.colors.WHITE12,
             content = ft.Column(
-            controls=[
-                ft.Row(
-                    controls=[
-                        ft.Text(value=str(self.main_ac.temperature), size=15),
-                        self.ac_mode_icon
-                    ]
-                )
-            ]
-        ))
+                controls=[
+                    ft.Row(
+                        controls=[
+                            ft.Text(value=str(self.main_ac.temperature)+'℃', size=15),
+                            self.ac_mode_icon,
+                            ft.Icon(name=ft.icons.MODE_FAN_OFF_OUTLINED, size=15),
+                            ft.Text(value=str(self.main_ac.mode), size=10),
+                        ]
+                    )
+                ]
+            )
+        )
+
+        # 設定温度入力ボックス
+        # TODO: max_lengthを指定して桁の制限を行う。
+        #   ※TextFieldにmax_lengthを指定すると、counter_textが自動表示されheightを専有する。
+        self.input_temperture = ft.Container(
+            padding = 0,
+            margin  = 0,
+            content = ft.Column(
+                controls=[
+                    ft.Row(
+                        controls=[
+                            ft.TextField(text_size=15,height=30, width=60, keyboard_type=ft.KeyboardType.NUMBER, max_lines=1, suffix_text='℃')
+                        ]
+                    )
+                ]
+            )
+        )
+
         return ft.WindowDragArea(
             ft.Container(
                 margin=0,
@@ -50,7 +72,8 @@ class SwitchBotRemote(ft.UserControl):
                         ft.Row(
                             controls=[
                                 self.ac_display,
-                                ft.IconButton(ft.icons.CLOSE, on_click=lambda _: self.page.window_close(), icon_size=15)
+                                self.input_temperture,
+                                ft.IconButton(ft.icons.CLOSE, on_click=lambda _: self.page.window_close())
                             ]
                         )
                         ]
@@ -65,14 +88,19 @@ class SwitchBotRemote(ft.UserControl):
         if self.main_ac.mode == 'heat':
             return ft.icons.SUNNY
         if self.main_ac == 'cool':
-            return ft.icons.SEVERE_COLD
+            return ft.icons.AC_UNIT
 
 def main(page: ft.Page):
     page.title = "Remote"
     page.window_height = 30
-    page.window_width  = 100
+    page.window_max_height = 30
+    page.window_width  = 350
     page.window_always_on_top = True
     page.window_frameless = True
+    page.window_maximizable = False
+    page.window_full_screen = False
+    page.window_resizable = False
+    page.window_skip_task_bar = True
     page.theme_mode = ft.ThemeMode.DARK
     page.padding = 0
     remote = SwitchBotRemote(page)
